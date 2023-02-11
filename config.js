4526aaca76c3418fc429cdb8c46eee75ba51ee92
Paul Merceur <paul.merceur83@gmail.com>
2023-02-11 19:22:50 +0100

    added db config

---
+++ config.js
@@ -0,0 +1,6 @@
+const { createClient } = require('@supabase/supabase-js')
+
+const supabase = createClient(
+    'https://ipnfajibmrzrpvypabkx.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwbmZhamlibXJ6cnB2eXBhYmt4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYxMzY2NDIsImV4cCI6MTk5MTcxMjY0Mn0.fQUzvqUU1v2q4PZwKh5nzOlP4jcLYnBHNFssWRcz8fw'
+  )
