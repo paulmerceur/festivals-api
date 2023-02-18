66b1f84e25b2a31566163de9139f584173fe4837
paul <paul.merceur83@gmail.com>
2023-02-18 21:29:22 +0100

    fixed creation functions

--- src/zones/controller.js
+++ src/zones/controller.js
@@ -29,12 +29,14 @@ router.getZoneById = async (req, res) => {
 
 // Create a new zone
 router.createZone = async (req, res) => {
+    const { nom } = req.body;
     try {
-        const { data: zone, error } = await supabase
+        const { data, error } = await supabase
             .from("zones")
-            .insert([{ ...req.body }]);
+            .insert([{ nom }])
+            .select("*")
         if (error) throw error;
-        res.status(200).json(zone);
+        res.status(201).json(data);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
