7526152e176fdbe8f798da669634b3312126f052
Paul Merceur <paul.merceur83@gmail.com>
2023-04-02 01:03:40 +0200

    micro change

--- src/benevoles/controller.js
+++ src/benevoles/controller.js
@@ -145,7 +145,7 @@ router.getAffectationsByBenevoleIdAndFestivalId = async (req, res) => {
     try {
         const { data, error } = await supabase
             .from("affectations")
-            .select(`zone(id, nom),
+            .select(`zone(id, nom, festival, nb_benevoles_min),
             creneau(id, date, heure_debut, heure_fin),
             benevole(user_id, prenom, nom, email, role),
             is_dispo,
